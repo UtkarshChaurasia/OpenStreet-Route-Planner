ba448c4d93ca5be1a9d6424e91368ef865af5621
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-05-11 15:00:28 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -82,7 +82,7 @@ int main(int argc, const char **argv)
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
